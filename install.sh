e88a6f9c53018cb14ee0030ff1661107fba0f865
Pidugu Likhil <pidugulikhil@gmail.com>
2026-03-11 17:13:16 -0400

    prefix addition stage

--- install.sh
+++ install.sh
@@ -2,12 +2,14 @@
 
 echo "[CYBREIGN] Installing core..."
 
+INSTALL_DIR="${PREFIX:-/usr/local}/bin"
+
 URL="https://raw.githubusercontent.com/cybreign/cybreign-core/main/cybreign"
 
-curl -L $URL -o $PREFIX/usr/local/bin/cybreign
+curl -L "$URL" -o "$INSTALL_DIR/cybreign"
 
-chmod +x $PREFIX/usr/local/bin/cybreign
+chmod +x "$INSTALL_DIR/cybreign"
 
 echo
-echo "CYBREIGN installed successfully."
+echo "CYBREIGN installed successfully!"
 echo "Run: cybreign help"

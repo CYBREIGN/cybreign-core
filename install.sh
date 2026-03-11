28790a557bc860c507139949bec23ef5d80d8e0b
Pidugu Likhil <pidugulikhil@gmail.com>
2026-03-11 17:04:23 -0400

    initial stage

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+echo "[CYBREIGN] Installing core..."
+
+URL="https://raw.githubusercontent.com/cybreign/cybreign-core/main/cybreign"
+
+curl -L $URL -o /usr/local/bin/cybreign
+
+chmod +x /usr/local/bin/cybreign
+
+echo
+echo "CYBREIGN installed successfully."
+echo "Run: cybreign help"
